be4699d66be830665103eb00e1911c883121c285
comdaeranh <eranh@comda.co.il>
2019-06-05 20:00:13 +0300

    Firebase - We need to test that the gradle sync and the project is sync to firebase

--- app/src/main/java/com/evyatartzik/android2_project/MainActivity.java
+++ app/src/main/java/com/evyatartzik/android2_project/MainActivity.java
@@ -3,12 +3,31 @@ package com.evyatartzik.android2_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
 }

3fd0e99eecc699dd6b9bc59814c806c8bba7fdef
EranHadad131 <38969897+EranHadad131@users.noreply.github.com>
2019-06-08 18:24:28 +0300

    strings updated

--- app/src/main/java/com/evyatartzik/android2_project/MainActivity.java
+++ app/src/main/java/com/evyatartzik/android2_project/MainActivity.java
@@ -5,23 +5,123 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.List;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView[] mDots;
+    private LinearLayout mDotsLayout;
+    private Button buttonNext;
+    private Button buttonPrev;
+    private int mCurrentPage;
+    ViewPager viewPager;
+    String phoneLanguage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Load*/
+        viewPager = findViewById(R.id.slideViewPager);
+        phoneLanguage = Locale.getDefault().getLanguage();
 
+        mDotsLayout = findViewById(R.id.dots_layout);
+        buttonNext = findViewById(R.id.button_Next);
+        buttonPrev = findViewById(R.id.button_Prev);
 
-        /*Load slider view */
-        ViewPager viewPager = findViewById(R.id.slideViewPager);
         SlideAdapter slideAdapter = new SlideAdapter(getSupportFragmentManager());
         viewPager.setAdapter(slideAdapter);
+        viewPager.addOnPageChangeListener(viewLisnter);
+        addDotIndicator(0);
     }
 
+    public void addDotIndicator(int position)
+    {
+        initButtons(position);
+
+        mDots = new TextView[3];
+        mDotsLayout.removeAllViews();
+        mDots = new TextView[3];
+        mDotsLayout.addView(buttonPrev);
+
+        for (int i=0;i<mDots.length;i++)
+        {
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(40);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+            mDotsLayout.addView(mDots[i]);
+        }
+        mDotsLayout.addView(buttonNext);
+
+        if( mDots.length>0) {
+            if (phoneLanguage == "iw") {
+                mDots[mDots.length - 1 - position].setTextColor(getResources().getColor(R.color.colorWhite));
+            } else if (phoneLanguage == "en") {
+                mDots[position].setTextColor(getResources().getColor(R.color.colorWhite));
+            }
+        }
+    }
+
+    private void initButtons(int position) {
+
+        if(position==0)
+        {
+            buttonNext.setEnabled(true);
+            buttonPrev.setEnabled(false);
+            buttonPrev.setVisibility(View.INVISIBLE);
+
+            buttonNext.setText("Next");
+            buttonPrev.setText("");
+
+        }
+        else if (position== mDots.length -1)
+        {
+            buttonNext.setEnabled(true);
+            buttonPrev.setEnabled(true);
+            buttonPrev.setVisibility(View.VISIBLE);
+
+            buttonNext.setText("Finish");
+            buttonPrev.setText("Back");
+        }
+        else
+        {
+            buttonNext.setEnabled(true);
+            buttonPrev.setEnabled(true);
+            buttonPrev.setVisibility(View.VISIBLE);
+
+            buttonNext.setText("Next");
+            buttonPrev.setText("Back");
+        }
+    }
+
+
+    ViewPager.OnPageChangeListener viewLisnter = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotIndicator(i);
+
+            Toast.makeText(MainActivity.this, "Swipe " + i + "", Toast.LENGTH_SHORT).show();
+            mCurrentPage = i;
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
 
 
     private class SlideAdapter extends FragmentStatePagerAdapter{

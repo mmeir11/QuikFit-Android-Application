3a6168a87964a7dfd7d1dc49cd70e6012246fb1c
evyacik <evyacik@gmail.com>
2019-07-02 12:06:55 +0300

    add activity class and user attributes

---
+++ app/src/main/java/com/evyatartzik/android2_project/Models/Activity.java
@@ -0,0 +1,80 @@
+package com.evyatartzik.android2_project.Models;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Activity {
+
+    private float latitude;
+    private float longitude;
+    private String type;
+    private ArrayList users;
+    private String title;
+    private Date date;
+    private String description;
+    private Boolean is_private;
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList users) {
+        this.users = users;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getIs_private() {
+        return is_private;
+    }
+
+    public void setIs_private(Boolean is_private) {
+        this.is_private = is_private;
+    }
+}
--- app/src/main/java/com/evyatartzik/android2_project/Models/User.java
+++ app/src/main/java/com/evyatartzik/android2_project/Models/User.java
@@ -1,16 +1,30 @@
 package com.evyatartzik.android2_project.Models;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class User {
 
     private String Name;
     private String Email;
     private String Password;
-    List<UserPreferences> userPreferences;
+    private ArrayList<UserPreferences> userPreferences;
+    private float latitude;
+    private float longitude;
+    private String profile_pic_path;
+    private UUID uuid;
+    private String about;
 
     public User(){ }
 
+    public User(String name,String email, String password,ArrayList<UserPreferences> userPreferences) {
+        Name = name;
+        Email = email;
+        Password = password;
+        this.userPreferences = userPreferences;
+        uuid = UUID.randomUUID();
+    }
     public String getName() {
         return Name;
     }
@@ -36,17 +50,13 @@ public class User {
     }
 
 
-    public User(String name,String email, String password,List<UserPreferences> userPreferences) {
-        Name = name;
-        Email = email;
-        Password = password;
-        this.userPreferences = userPreferences;
-    }
-    public List<UserPreferences> getUserPreferences() {
+    public ArrayList<UserPreferences> getUserPreferences() {
         return userPreferences;
     }
 
-    public void setUserPreferences(List<UserPreferences> userPreferences) {
+    public void setUserPreferences(ArrayList<UserPreferences> userPreferences) {
         this.userPreferences = userPreferences;
     }
+
+
 }
